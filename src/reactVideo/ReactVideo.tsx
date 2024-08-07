7b6eb55d66c4192918b9e6f0be634fec1ed9ec9e
Arghya Maity <maityarghya0@gmail.com>
2024-08-07 16:31:01 +0530

    primary setup done

---
+++ src/reactVideo/ReactVideo.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./reactVideo.css";
+
+function ReactVideo() {
+  return <div className="video-container">ReactVideo</div>;
+}
+
+export default ReactVideo;
---
+++ src/reactVideo/index.tsx
@@ -0,0 +1,1 @@
+export { default as ReactVideo } from "./ReactVideo";
